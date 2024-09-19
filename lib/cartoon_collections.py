31bdbf6fb8ee3333138707065057f5f7543d5fc2
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-09-18 22:28:25 -0400

    passes all tests!

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    [print(f"{i + 1}. {dwarves[i]}") for i in range(len(dwarves))]
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteers):
+    newlist = [planeteers[i].capitalize() + "!" for i in range(len(planeteers))]
+    return newlist
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(planeteers):
+    for planeteer in planeteers:
+        if len(planeteer) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(ingredients):
+    for ingredient in ingredients:
+        if ingredient in ["camembert", "gouda", "cheddar"]:
+            return ingredient
+    return None
